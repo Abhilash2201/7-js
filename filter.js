6d34ba9c1b780d29b870e412e25c4f94bee923e7
Abhilash2201 <abhilashl9845@gmail.com>
2023-11-10 19:25:28 +0530

    hi

---
+++ filter.js
@@ -0,0 +1,462 @@
+var product=[
+  {id:1,
+      pname:"Apple",
+      pcategory:"electronics",
+      img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-bluetitanium?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1692846360609",
+      price:"25000",
+      productdiscribition:"It is good product for all types of users",
+      },
+  {id:2,
+          pname:"Nokia",
+          pcategory:"Electronis",
+          img:"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSSxH7gwImqjRLQ-xVNogf5r_fB30BkD-5V2WAp11SFurwT_4M6fub4HVsS3oQUZyBXGMN_vmYRrZUy0045pwgzWqKIjAFU5yKINLYoU5ZzaOyROxCXpLhNQZ0A72MnrDMFla1jPlXltQ&usqp=CAc",
+          price:"2500",
+          productdiscribition:"It is good Good phone",
+  },
+  {id:3,
+      pname:"VIvo",
+      pcategory:"Electronis",
+      img:"https://exstatic-in.vivo.com/Oz84QB3Wo0uns8j1/in/1695965930777/3ff40e025f5eb728acdd3af13b3b15a9.png.webp",
+      price:"2500",
+      productdiscribition:"It is good Good phone",
+   },
+  {id:4,
+      pname:"onepluse",
+      pcategory:"Electronis",
+      img:"https://lh3.googleusercontent.com/spp/ABYooDqLH7g5vduVVDes30G2sp07zXSvbxXCSGhKROFyFS1gzad4MjnuAxsfd_oNFcWdWhW8DiVb4T63kB4iz-qZ4ERwxEXhvmZUifXUEv0l_acM_gGVroMcoscdMIufqqXC4oLRoLmXMaYljUbW67fGwE6ZLAVoOE86NEzVpiwFyqERUVCwsU03BRRDuHPQN3uJkkOcSHRXgeU=s512-rw-pd-pc0x00ffffff",
+      price:"18000",
+      productdiscribition:"It is good Good phone",
+  },
+  {id:5,
+      pname:"Samsung",
+      pcategory:"Electronis",
+      img:"https://lh3.googleusercontent.com/spp/ABYooDqcVSbQJLrT7hKqrH2wCaw0oMG1xjVXajM9TqR-glxLlMgmkXmZxlZPmGap-cjzEQgJPKa0QnHAjw8VbFafoK_2dEtgqWvZGxAl-BBwLT1X3nVem_RxtpunG-1oyopq9yINuwtatDMxnZGq3umP_-gVnQYisCXEHCGxXUcRfMKt2AHwPDs9lOaxpTelycq4GPwL2Lw=s512-rw-pd-pc0x00ffffff",
+      price:"20000",
+      productdiscribition:"It is good Good phone",
+  } , 
+  { id:6,
+      pname:"Realme",
+      pcategory:"Electronis",
+      img:"https://lh3.googleusercontent.com/spp/ABYooDquz3Q6_b3zipWdo4saoVQuka2-lKZaBzyaLSeMXJBzVrTBKC8P4FCR0nlkpPxysyPzA02Cdsrp6GTOzS29BIkRsBC9w_fHTTM7s7kXfzBT6DR_Ms3c_X2Gxi3PtzHt7yCpPAexCQhlamkoOtSFSgAU37-rowAG1rQInNU5-L9AfM3nR2tyAJ8YZ9NWZkwHMW9PI5jKfw=s512-rw-pd-pc0x00ffffff",
+      price:"19000",
+      productdiscribition:"It is good Good phone",
+  } ,
+  {  id:7,
+      pname:"xiaomi",
+      pcategory:"Electronis",
+      img:"https://lh3.googleusercontent.com/spp/ABYooDq2frM0gkkdOBKtfO6TmrI5giUQgp4_bY17V6o1xwuCmfzw0D7-gs5IHMowH5diLr9PPa0P_Ml-TSXexnI2GHJffBi3W9Fuma878DlTbaOAZZiETrQB5smos9aoVrRZ2k1sEHXx1hJ-O6u4NR4SSUymGYuf6SsKcgUXE5aD6JyWQuSNs0-MuA_cdJDNq0t9dd6cH3_y=s512-rw-pd-pc0x00ffffff",
+      price:"25800",
+      productdiscribition:"It is good Good phone",
+  },
+  { id:8,
+      pname:"Google Pixel 8 Pro",
+      pcategory:"Electronis",
+      img:"https://lh3.googleusercontent.com/spp/ABYooDq2ZMONKAs-N9Oi36qTPruY42-4XS_DDdY_NGG969HQU1YZEU9vtMgjGbv1Sun6Jvi3P4mlm7YIoa0KQ8STq2yFeP2Zxr4kbmVXLxStoxazYXSKpwhn4ynePekfV-yOml0YovXa0wZF71PpQWPI-sK1VcyS71Vq5ValK1fnc6yQOLSmo8Q3UnsDXPna53urqyqF8VTN0g=s512-rw-pd-pc0x00ffffff",
+      price:"25000",
+      productdiscribition:"It is good Good phone",
+  },
+  { id:9,
+      pname:"oopo number 1",
+      pcategory:"Electronis",
+      img:"https://lh3.googleusercontent.com/spp/ABYooDpwyYAlfyBMt2BOnBh2mVYqOApTErOJFL3wQGTKWs-YgSDO51Narwm_KdGASMnJRM2O3HUCyh3QvGnDFz3ycS_34VjUF4lfLIFyVrCUg88i22tFsX5pUOcEuFs-3FneP_mM0OiqUyQusTMaUjif1OhRdjqhdThGfQZOM_XccpjYbZv2Q01E2AUc3IGTasd1YVYStKQTIg=s512-rw-pd-pc0x00ffffff",
+      price:"2500",
+      productdiscribition:"It is good Good phone",
+  },
+  { id:10,
+      pname:"iQOO Neo 7 Pro",
+      pcategory:"Electronis",
+      img:"https://www.91-img.com/pictures/156864-v2-iqoo-neo-7-pro-mobile-phone-large-1.jpg?tr=q-80",
+      price:"2500",
+      productdiscribition:"It is good Good phone",
+  },
+  
+  
+  
+  
+  
+  { id:11,
+      pname:"formal",
+      pcategory:"Mens fashion",
+      img:"https://i.etsystatic.com/28865767/r/il/7831ba/4032898909/il_570xN.4032898909_ahxh.jpg",
+      price:"2500",
+      productdiscribition:"It is good Good product for mens were",
+  },
+  { id:12,
+      pname:"Shorts",
+      pcategory:"Mens fashion",
+      img:"https://images.meesho.com/images/products/318044366/nheve_512.jpg",
+      price:"2500",
+      productdiscribition:"It is good Good product for mens were",
+  },
+  { id:13,
+      pname:"T-Shirt",
+      pcategory:"Mens fashion",
+      img:"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1687842433_3385347.jpg?format=webp&w=480&dpr=1.3",
+      price:"2500",
+      productdiscribition:"It is good Good product for mens were",
+  },
+  { id:14,
+      pname:"Shirt",
+      pcategory:"Mens fashion",
+      img:"https://fashionfiver.in/image/cache/catalog/shirt/Men%20Plain%20Shirts%20wine%20color-1000x1000.jpeg",
+      price:"2500",
+      productdiscribition:"It is good Good product for mens were",
+  },
+  { id:15,
+      pname:"pant",
+      pcategory:"Mens fashion",
+      img:"https://img3.junaroad.com/uiproducts/20396437/zoom_1-1696990403.jpg",
+      price:"2500",
+      productdiscribition:"It is good Good product for mens were",
+  },
+  { id:16,
+      pname:"Night were",
+      pcategory:"Womens fashion",
+      img:"https://images.meesho.com/images/products/216620872/qryio_512.webp",
+      price:"2500",
+      productdiscribition:"It is good Good product for womens were",
+  },
+  { id:17,
+      pname:"saree",
+      pcategory:"Womens fashion",
+      img:"https://www.latestkurtidesigns.com/wp-content/uploads/2019/07/blog2-1-708x1024.jpg",
+      price:"2500",
+      productdiscribition:"It is good Good product for womens were",
+  },
+  { id:18,
+      pname:"western Top",
+      pcategory:"Womens fashion",
+      img:"https://images.meesho.com/images/products/101811925/5rupe_512.webp",
+      price:"2500",
+      productdiscribition:"It is good Good product for womens were",
+  },
+  { id:19,
+      pname:"pant",
+      pcategory:"Womens fashion",
+      img:"https://assets.ajio.com/medias/sys_master/root/20230628/tQb4/649bd4b2eebac147fc214e67/-473Wx593H-465934260-black-MODEL.jpg",
+      price:"2500",
+      productdiscribition:"It is good Good product for womens were",
+  },
+  { id:20,
+      pname:"kutra",
+      pcategory:"Womens fashion",
+      img:"https://absolutelydesi.com/wp-content/uploads/2023/04/Lavanya-The-Label-Women-Yoke-Design-Anarkali-Pure-Cotton-Kurta-Set-5.jpg",
+      price:"2500",
+      productdiscribition:"It is good Good product for womens were",
+  },
+  
+  
+  
+  
+  
+  { id:21,
+      pname:"circket BAT",
+      pcategory:"sports",
+      img:"https://stage.dsc-cricket.com/media/catalog/product/cache/6d9f53a3f26f3a62c1c1c9add7c6ad94/b/u/bull-31-david-warner-english-willow-cricket-bat.jpg",
+      price:"",
+      productdiscribition:"It is very good and strong bat",
+  },
+  { id:22,
+      pname:"circket ball",
+      pcategory:"sports",
+      img:"https://media.istockphoto.com/id/181668903/vector/shiny-red-traditional-cricket-ball.jpg?s=612x612&w=0&k=20&c=FrODP12OH_uCIhzpa9oChPu6l--5h5BBLgnkJy9n5sg=",
+      price:"1000",
+      productdiscribition:"It is very good and strong ball",
+  },
+  { id:23,
+      pname:"hockey",
+      pcategory:"sports",
+      img:"https://m.media-amazon.com/images/I/41qqhMybyaL._AC_UF894,1000_QL80_.jpg",
+      price:"2000",
+      productdiscribition:"it is very good and strong stict",
+  },
+  { id:24,
+      pname:"basket ball",
+      pcategory:"sports",
+      img:"https://upload.wikimedia.org/wikipedia/commons/7/7a/Basketball.png",
+      price:"24900",
+      productdiscribition:"it is very good ball",
+  },
+  { id:25,
+      pname:"football",
+      pcategory:"sports",
+      img:"https://upload.wikimedia.org/wikipedia/commons/1/1d/Football_Pallo_valmiina-cropped.jpg",
+      price:"1234",
+      productdiscribition:"it izsxdcfgvhjnertyu good",
+  },
+  { id:26,
+      pname:"volley ball",
+      pcategory:"sports",
+      img:"https://img.freepik.com/free-photo/ball-hitting-net_23-2149450951.jpg?w=1060&t=st=1699455724~exp=1699456324~hmac=8ba96604489b9faea2f948bbbcdc2940574b5c97aed27bb6fea672dad512caea",
+      price:"3456",
+      productdiscribition:"dfvghbjnmkwsedrtfyugnkm",
+  },
+  { id:27,
+      pname:"shuttle cock",
+      pcategory:"sports",
+      img:"https://cdn.shopify.com/s/files/1/0020/9407/1890/files/shuttles-close-up_480x480.jpg?v=1559295221",
+      price:"987654",
+      productdiscribition:"wesxrdcftvgbhunjmlmk,jmhgdfdxcvgbh",
+  },
+  { id:28,
+      pname:"table tennis",
+      pcategory:"sports",
+      img:"https://static.toiimg.com/thumb/resizemode-4,width-1200,height-900,msid-100824676/100824676.jpg",
+      price:"123456",
+      productdiscribition:"wexrtyuhijljkjgdxcvbnm,esrdtyguhjiml",
+  },
+  { id:29,
+      pname:"long jump",
+      pcategory:"sports",
+      img:"https://img.freepik.com/free-vector/long-jump-girl-jumping-sport-event_1308-31818.jpg",
+      price:"1234",
+      productdiscribition:"poiuytrewadfxcgvhjm,nmbvaesrdtfygjhkljk",
+  },
+  
+  { id:30,
+      pname:"short put",
+      pcategory:"sports",
+      img:"https://www.liveabout.com/thmb/1vkCBnMkiVwG_ssS-tM9NDFIVdg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-101882298-5905f4ad3df78c5456f8e9ca.jpg",
+      price:"123",
+      productdiscribition:"awsedfghuijok;lkjhgfdzs vbnm,oiuwasdfghjbknm",
+  },
+  
+  
+  
+  
+  
+  
+  { id:31,
+      pname:"Lakme",
+      pcategory:"Beauty fashion",
+      img:"https://m.media-amazon.com/images/I/31LdCo2u8eL._SX300_SY300_QL70_FMwebp_.jpg",
+      price:"12345",
+      productdiscribition:"qawerdtfyuhijk;,.mnmdcvbnm",
+  },
+  { id:32,
+      pname:"nykee",
+      pcategory:"Beauty fashion",
+      img:"https://images-static.nykaa.com/uploads/7a2854f7-41b5-46af-8ea4-3f6860404999.jpg?tr=w-240,cm-pad_resize",
+      price:"345678",
+      productdiscribition:"wexrctvybunmk,l;.xctfvgybhnjkm",
+  },
+  
+  { id:33,
+      pname:"purple",
+      pcategory:"Beauty fashion",
+      img:"https://media6.ppl-media.com/tr:h-250,w-250,c-at_max/static/img/product/279845/lakme-perfect-radiance-fairness-face-wash-50-g-14_2_display_1651745531_6531dafe.jpg",
+      price:"34567",
+      productdiscribition:"zxdctfvygbuhnijmk,l;. mnbvsedrftgyhuijok",
+  },
+  { id:34,
+      pname:"eye linear",
+      pcategory:"Beauty fashion",
+      img:"https://www.lakmeindia.com/cdn/shop/files/22810_H1_8901030763199_5353770a-d644-4e12-b50f-1060ea8e6db0_600x.jpg?v=1682485010",
+      price:"56789",
+      productdiscribition:"",
+  },
+  { id:35,
+      pname:"lip boder",
+      pcategory:"Beauty fashion",
+      img:"https://www.lakmeindia.com/cdn/shop/products/29373_H-8901030916014_600x.jpg?v=1669891761",
+      price:"3456789",
+      productdiscribition:"wertfyguhijokpl;,lxtcyvubinom",
+  },
+  
+  { id:36,
+      pname:"perfurm",
+      pcategory:"Beauty fashion",
+      img:"https://www.lakmeindia.com/cdn/shop/files/27765_H-8901030960208_600x.jpg?v=1689579270",
+      price:"12345",
+      productdiscribition:"awsedrftgyhujio,lkj hxwsedrftgyhujiokp",
+  },
+  { id:37,
+      pname:"primer",
+      pcategory:"Beauty fashion",
+      img:"https://www.lakmeindia.com/cdn/shop/files/24451_H_8901030932861_600x.jpg?v=1698911722",
+      price:"2345",
+      productdiscribition:"wsedrtfyguhijokpl.;lmkjrdtyguijo",
+  },
+  { id:38,
+      pname:"concner",
+      pcategory:"Beauty fashion",
+      img:"https://media6.ppl-media.com/tr:h-750,w-750,c-at_max,dpr-2/static/img/product/246375/the-face-shop-rice-and-ceramide-moisturizing-cream-50-ml-60_10_display_1675414844_049e99b8.jpg",
+      price:"6780",
+      productdiscribition:"xcvybuinml,kj dzwesdrf67g8h9j0kpl[;,lcf",
+  },
+  { id:39,
+      pname:"fondation",
+      pcategory:"Beauty fashion",
+      img:"https://media6.ppl-media.com/tr:h-750,w-750,c-at_max,dpr-2/static/img/product/319981/estee-lauder-double-wear-stay-in-place-makeup-spf-10-1n1-ivory-nude-30-ml_3_display_1616137823_74111d76.jpg",
+      price:"23489",
+      productdiscribition:"xrctybuiom,;.lk jhgfsedrftg7yh8ujikopl[",
+  },
+  { id:40,
+      pname:"eye kajal",
+      pcategory:"Beauty fashion",
+      img:"https://www.lakmeindia.com/cdn/shop/files/LakmeColorCrushNailArtValueSet_600x.jpg?v=1688619300",
+      price:"4568",
+      productdiscribition:"qawsedrftgyhujikopl,lmknjhbgvfcdxwasedrftgyhuijo",
+  },
+  
+  
+  
+  
+  
+  { id:41,
+      pname:"Dolo650",
+      pcategory:"Medecine",
+      img:"https://tse2.mm.bing.net/th?id=OIP.qiF7cjl-GFHHDXfi1o7IhwHaD4&pid=Api&P=0&h=180",
+      price:"123",
+      productdiscribition:"xctvybuimo,p.",
+  },
+  { id:42,
+      pname:"parcetamal",
+      pcategory:"Medecine",
+      img:"https://tse1.mm.bing.net/th?id=OIP.d_6RXx27onGAed_GLsszlgHaFX&pid=Api&P=0&h=180",
+      price:"467",
+      productdiscribition:"",
+  },
+  { id:43,
+      pname:"senrest",
+      pcategory:"Medecine",
+      img:"https://tse3.mm.bing.net/th?id=OIP.33GZwvWAv0E05jK9VIg02AHaEK&pid=Api&P=0&h=180",
+      price:"568",
+      productdiscribition:"zexrctybuimo,p.;l,kjhgfcxertfyguhijom,",
+  },
+  { id:44,
+      pname:"de cough",
+      pcategory:"Medecine",
+      img:"https://tse2.mm.bing.net/th?id=OIP.HErXzhoXsTxAV9HojrsrYwHaHa&pid=Api&P=0&h=180",
+      price:"567",
+      productdiscribition:"erctvybuimo,p.;l kjhgszxdrctvybunimo,",
+  },
+  { id:45,
+      pname:"hebern syrup",
+      pcategory:"Medecine",
+      img:"https://tse4.mm.bing.net/th?id=OIP.vvLBgao6atfvdrBITYF4LgHaHa&pid=Api&P=0&h=180",
+      price:"67",
+      productdiscribition:"zxerctybuiom,plmkjnhbgvfcxerf6tg7h8j9ko",
+  },
+  
+  { id:46,
+      pname:"Dabur hontiner",
+      pcategory:"Medecine",
+      img:"https://images.freekaamaal.com/post_images/1589888340.jpg",
+      price:"567",
+      productdiscribition:"zwexrctvybunimo,pl.;l,kmjnhbgvfcdxswxecrvtbyunim",
+  },
+  { id:47,
+      pname:"nyal sypur",
+      pcategory:"Medecine",
+      img:"https://tse3.mm.bing.net/th?id=OIP.XpGKR-O5IQikcsQG7tTeFAHaHa&pid=Api&P=0&h=180",
+      price:"567",
+      productdiscribition:"qwsedrtvybunimo,p.;l,kmjnhbgvcdxzexrctvybunim",
+  },
+  { id:48,
+      pname:"heart beat",
+      pcategory:"Medecine",
+      img:"https://tse4.explicit.bing.net/th?id=OIP.sU3aOJ4C9_erDd8srCyNJwHaHa&pid=Api&P=0&h=180",
+      price:"568",
+      productdiscribition:"zxecrtvybunimk,lk bvcxzawertyui,nbvcxz",
+  },
+  { id:49,
+      pname:"heartcare",
+      pcategory:"Medecine",
+      img:"https://tse4.mm.bing.net/th?id=OIP.7Cm0nFIHwtCcxBB1faMQkgHaHa&pid=Api&P=0&h=180",
+      price:"678",
+      productdiscribition:"zxcvbnmvcxzaqwsdfoklmknjseduijo",
+  },
+  { id:50,
+      pname:"Himayala care",
+      pcategory:"Medecine",
+      img:"https://tse4.mm.bing.net/th?id=OIP.W0huHHRg2z7PEzXTZ7Mo4QHaGv&pid=Api&P=0&h=180",
+      price:"567",
+      productdiscribition:"lkjhgfdsaqwertyuioplmnbvcxzas",
+  }, 
+  { id:51,
+    pname:"karnataka care",
+    pcategory:"Medecine",
+    img:"https://cdn.pixabay.com/photo/2023/10/28/06/40/wine-8346641_640.jpg",
+    price:"567",
+    productdiscribition:"lkjhgfdsaqwertyuioplmnbvcxzas",
+}
+  ]
+ 
+  
+var my_div=document.querySelector(".main-container")  
+var btn=document.querySelector("#btn")  
+var fil=document.querySelectorAll(".fil")  
+console.log(fil);
+fil.forEach((ele)=>{
+  ele.addEventListener("click",()=>{
+    console.log("i was clicked");
+    console.log(ele.dataset.val);
+     var inf=ele.dataset.val
+   
+       var fitered =product.filter((elem)=>{
+       return  elem.pcategory=="Medecine"
+      })  
+  console.log(fitered);
+  my_div.innerHTML=""
+  fitered.forEach((ele)=>{
+   
+    my_div.innerHTML+=`<aside class="each-item">
+    <img src=${ele.img} alt=${ele.pname} >
+    <div class="price">
+        <p>${ele.pcategory}</p>
+        <p>${ele.pname}</p>
+        <p>${ele.price}</p>
+    </div>
+    <div class="desc">
+        <p>${ele.productdiscribition}</p>
+    </div>
+    </aside>`
+
+  })
+  
+
+  })
+})
+
+
+
+
+
+
+  window.addEventListener("DOMContentLoaded",()=>{
+//  product.forEach((ele)=>{
+//   my_div.innerHTML+=`<aside class="each-item">
+//   <img src=${ele.img} alt=${ele.pname} >
+//   <div class="price">
+//       <p>${ele.pcategory}</p>
+//       <p>${ele.pname}</p>
+//       <p>${ele.price}</p>
+//   </div>
+//   <div class="desc">
+//       <p>${ele.productdiscribition}</p>
+//   </div>
+//   </aside>`
+
+var ans=product.map((ele)=>{
+ return `<aside class="each-item">
+  <img src=${ele.img} alt=${ele.pname} >
+  <div class="price">
+      <p>${ele.pcategory}</p>
+      <p>${ele.pname}</p>
+      <p>${ele.price}</p>
+  </div>
+  <div class="desc">
+      <p>${ele.productdiscribition}</p>
+  </div>
+  </aside>`
+  
+ })
+ 
+// console.log(ans);
+// console.log(ans.join(""));
+my_div.innerHTML=ans.join(",")
+
+  })
+  
+  
